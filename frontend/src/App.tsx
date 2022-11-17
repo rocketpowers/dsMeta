5d923fef09a7a017034b99300564fadcf8452346
rocketpowers <rocketpowerswillian@gmail.com>
2022-11-17 18:31:18 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
- 
-  return (
 
-  <h1>hello word</h1>
-  
+  return (
+    <>
+      <h1>hello word</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+
   )
 
 }
