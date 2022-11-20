0ea8f15336ac73714d332b5986b0b715cb3904ed
rocketpowers <rocketpowerswillian@gmail.com>
2022-11-20 09:43:01 -0300

    dataBase create and seed

---
+++ backend/src/main/java/dsMetaPackages/entities/Sale.java
@@ -0,0 +1,33 @@
+package dsMetaPackages.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="tb_sales")
+public class Sale {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	public Sale() {
+		
+	}
+	
+
+}
