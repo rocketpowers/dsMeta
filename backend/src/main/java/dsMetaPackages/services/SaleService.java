d58698ac13136570baba276e3375f5e7add43770
rocketpowers <rocketpowerswillian@gmail.com>
2023-01-13 15:18:00 -0300

    date select fixed up

--- backend/src/main/java/dsMetaPackages/services/SaleService.java
+++ backend/src/main/java/dsMetaPackages/services/SaleService.java
@@ -1,10 +1,9 @@
 package dsMetaPackages.services;
 
-
-
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,27 +13,21 @@ import org.springframework.stereotype.Service;
 import dsMetaPackages.entities.Sale;
 import dsMetaPackages.repository.SaleRepository;
 
-
 @Service
 public class SaleService {
-	
+
 	@Autowired
 	private SaleRepository repository;
-	
-	//public List<Sale> findSales() {
+
 	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
-		
+
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-		
-		LocalDate min=LocalDate.parse(minDate);
-		LocalDate max=LocalDate.parse(maxDate);
-		
-		return repository.findSales(min,max, pageable);
-		
-		
-		
-		
-		
+
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+		return repository.findSales(min, max, pageable);
+
 	}
 
 }
---
+++ backend/src/main/java/dsMetaPackages/services/SmsService.java
@@ -0,0 +1,47 @@
+package dsMetaPackages.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import dsMetaPackages.entities.Sale;
+import dsMetaPackages.repository.SaleRepository;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+
+	@Autowired
+	private SaleRepository saleRepository;
+
+	public void sendSms(Long saleId) {
+		
+		Sale sale = saleRepository.findById(saleId).get();
+		
+		String msg = "vendedor" + sale.getSellerName();
+
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, "teste").create();
+
+		System.out.println(message.getSid());
+	}
+}
